665307a170c1eab5df23d915102b5d878519f9c5
Souvik <dronser8@gmail.com>
2021-06-15 18:35:47 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'soft-black': '#131416',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
